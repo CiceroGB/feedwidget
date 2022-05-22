a665711ea992dac1254dc93890b6f6fdaeaf5599
Cicero Gasparini Brunelli <brunelli.cg@gmail.com>
2022-05-22 16:57:36 -0300

    feat: cors and able request json limit 50mb

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from 'cors'
 import { routes } from "./route";
 
 const app = express();
-app.use(express.json());
+app.use(cors())
+app.use(express.json({limit: '50mb'}));
 app.use(routes);
 
 app.listen(3333, () => console.log("server running"));
